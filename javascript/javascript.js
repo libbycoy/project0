fb52fd93c299ddcb2e79893215308ef9c280129c
Elizabeth Coy <elizabethcoy@Elizabeths-MacBook-Pro.local>
2016-09-22 10:40:59 +1000

    updates from luke

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,10 +2,68 @@ $( document ).ready( function() {
 
   var turn = 0;
 
-  function reset() {
+  var winner = '';
+
+  var testForWin = function () {
+
+    var num1 = $('#name1').text();
+    var num2 = $('#name2').text();
+    var num3 = $('#name3').text();
+    var num4 = $('#name4').text();
+    var num5 = $('#name5').text();
+    var num6 = $('#name6').text();
+    var num7 = $('#name7').text();
+    var num8 = $('#name8').text();
+    var num9 = $('#name9').text();
+
+    if ((num1 && num1 === num2) && (num2 === num3)) {
+    winner = num1;
+  } else if ((num2 && num2 === num5) && (num5 === num8)) {
+    winner = num2;
+  } else if ((num3 && num3 === num6) && (num6 === num9)) {
+    winner = num3;
+  } else if ((num1 && num1 === num4) && (num4 === num7)) {
+    winner = num1;
+  } else if ((num1 && num1 === num5) && (num5 === num9)) {
+    winner = num1;
+  } else if ((num3 && num3 === num5) && (num5 === num7)) {
+    winner = num3;
+  }
+
+  };
+
+  var playMove = function (cell) {
+
+      if ( $(cell).text().length === 1 ) {
+        return;
+      }
+
+      if ( turn % 2 == 0 ) {
+        $(cell).val('x');
+        $(cell).text('x');
+
+      } else {
+        $(cell).val('o');
+        $(cell).text('o');
+
+     }
+
+      if ( turn === 8 ) {
+        console.log('No one has won! Start again.')
+        reset();
+      }
+
+
+      turn++;
+
+      testForWin();
+
+  };  // end playMove()
+
+  var reset = function () {
     turn = 0;
-    $('td').text('click').val('');
-   };
+    $('td').text('click');
+  };
 
   $('button').on('click', function() {
     reset();
@@ -13,37 +71,12 @@ $( document ).ready( function() {
 
   $('td').on('click', function() {
 
+    // play move function
+    playMove(this);
 
-  var num1 = $('#name1').val();
-  var num2 = $('#name2').val();
-  var num3 = $('#name3').val();
-  var num4 = $('#name4').val();
-  var num5 = $('#name5').val();
-  var num6 = $('#name6').val();
-  var num7 = $('#name7').val();
-  var num8 = $('#name8').val();
-  var num9 = $('#name9').val();
-
-
-
-  if ( $(this).text().length === 1 ) {
-    return;
-  }
-
-  if ( turn % 2 == 0 ) {
-    $(this).val('x');
-    $(this).text('x');
-
-  } else {
-    $(this).val('o');
-    $(this).text('o');
-
- }
-  if ( turn === 8 ) {
-    reset();
-  }
-
-  turn ++;
+    if ($(winner).text().length === 1) {
+    $('#message').html('winner is ' + winner );
+  };
   });
 
 });
---
+++ markdown/README.md
@@ -0,0 +1,38 @@
+# Markdown Intro in h1
+
+We can have **bold** text and *italic* text. Underscores __also__ _work_.
+
+[Link to GA](http://generalassembl.ly/)
+
+http://yahoo.com/
+
+make a link to the readme file in the html of the live site.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+
+## More Details in h2
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+## Lists
+### Unordered
+* Groucho
+* Harpo
+* Chico
+
+### Ordered
+1. Groucho
+2. Harpo
+3. Chico
+
+![Bill Murray](http://fillmurray.com/150/150)
+
+As a wise man once said:
+> ALL YOUR BASE ARE BELONG TO US
+
+Javascript has many quirks such as `Nan`
